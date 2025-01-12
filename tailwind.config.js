30aa0cc54492e192133dc8732dce3eccfe7cfd5d
itzNamanDev <namanmemes@gmail.com>
2025-01-12 14:04:13 +0545

    lol
    lol

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./*.html", // Include all HTML files in the root directory
+    "./src/**/*.html", // Include HTML files in the src folder (if any)
+    "./src/**/*.js",   // Include JavaScript files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
